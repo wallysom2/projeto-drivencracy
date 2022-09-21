fed088770fe7db8530376788b473b2e8af225207
Matheus <wallysom2@gmail.com>
2022-09-21 14:54:27 -0300

    feat: subindo a api no Heroku

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const app = express();
 
 connectDatabase();
 app.use(express.json());
+
+app.get ("/", (req,res) => {
+    res.send ("Tudo ok")
+})
 
 app.listen(port, () => console.log(`Server running on port: ${port}`));
