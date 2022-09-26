32e16bf4899230162a683a61f8cf2d0997327058
Matheus <wallysom2@gmail.com>
2022-09-26 11:15:45 -0300

    feat: criando rota poll choice e conctando-a ao controller

--- src/routes/poll.route.js
+++ src/routes/poll.route.js
@@ -1,10 +1,11 @@
 import {Router} from "express";
 
-import {getPoll, addPoll} from "../controllers/poll.controller.js";
+import {getPoll, addPoll, getPollChoices} from "../controllers/poll.controller.js";
 
 const pollRouter = Router();
 
 pollRouter.get("/poll", getPoll);
 pollRouter.post("/poll", addPoll);
+pollRouter.get("/poll/:id/choice/", getPollChoices);
 
 export default pollRouter;
