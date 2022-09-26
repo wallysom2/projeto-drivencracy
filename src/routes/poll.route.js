77ccbc7fd6d01bd8bbd4a0d740beb6508e7e1435
Matheus <wallysom2@gmail.com>
2022-09-26 11:32:31 -0300

    feat: configurando rota de acesso ao resultado da enquete

--- src/routes/poll.route.js
+++ src/routes/poll.route.js
@@ -1,11 +1,12 @@
 import {Router} from "express";
 
-import {getPoll, addPoll, getPollChoices} from "../controllers/poll.controller.js";
+import {getPoll, addPoll, getPollChoices, getPollResults} from "../controllers/poll.controller.js";
 
 const pollRouter = Router();
 
 pollRouter.get("/poll", getPoll);
 pollRouter.post("/poll", addPoll);
 pollRouter.get("/poll/:id/choice/", getPollChoices);
+pollRouter.get("/poll/:id/result", getPollResults);
 
 export default pollRouter;
